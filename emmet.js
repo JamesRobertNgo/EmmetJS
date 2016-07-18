48e8fe587818e990ab55aa7dd9ebf8cc997c69da
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2016-07-17 21:22:41 -0400

    Refactor

--- emmet.js
+++ emmet.js
@@ -1,306 +1,322 @@
 
-window.emmetJS = function(emmetStr) {
-	
-	// ---
-	
-	function getOperation(nodes, emmetStr) {
-		
-		var nextNodes = null;
-		
-		var match = /[\>\+\^\*#\.\[\{]/.exec(emmetStr);
-		
-		if (!match) // NO MATCH
-			return;
-		
-		// FORK BASED ON (FIRST) MATCHED
-		
-		switch (match[0]) {
-		case '>':
-			
-			// CHILD
-			
-			nextNodes = getElement(emmetStr);
+// ==========
+// NAMESPACE
+// ==========
 
-			for (var i = 0; i < nodes.length; i++)
-				for (var j = 0; j < nextNodes.length; j++)
-					nodes[i].appendChild(nextNodes[j].cloneNode(true));
-			
-			break;
-		
-		case '+':
-			
-			// SIBLING
-			
-			nextNodes = getElement(emmetStr);
+window.emmet = {}
 
-			for (var i = 0; i < nextNodes.length; i++)
-				nodes.push(nextNodes[i].cloneNode(true));
-			
-			break;
-		
-		case '^':
-			
-			// CLIMB-UP (PART 1)
-			
-			nextNodes = getElement(emmetStr);
-			
-			nodes.climbup = nextNodes;
-			
-			break;
-		
-		case '*':
-			
-			// MULTIPLICATION
-			
-			var matchParam = /[0-9]+/.exec(emmetStr);
-			
-			if (!matchParam)
-				break;
-			
-			var copyNodes = [];
-			
-			for (var i = 0; i < nodes.length; i++)
-				copyNodes.push(nodes[i].cloneNode(true));
-			
-			for (var i = 0; i < parseInt(matchParam[0]) - 1; i++)
-				for (var j = 0; j < copyNodes.length; j++)
-					nodes.push(copyNodes[j].cloneNode(true));
-			
-			emmetStr = emmetStr.substring(matchParam.index + matchParam[0].length);
-			
-			emmet.getOperation(nodes, emmetStr);
-			
-			break;
-		
-		case '#':
-			
-			// ID ATTRIBUTE
-			
-			emmetStr = emmetStr.substring(match.index + 1);
-			
-			var textMatch = /[a-zA-Z0-9_\-]+/.exec(emmetStr);
-			
-			emmetStr = emmetStr.substring(textMatch.index + textMatch[0].length);
-			
-			if (!textMatch)
-				break;
-			
-			for (var i = 0; i < nodes.length; i++)
-				nodes[i].setAttribute('id', textMatch[0]);
-			
-			emmet.getOperation(nodes, emmetStr);
-			
-			break;
-		
-		case '.':
-			
-			// CLASS ATTRIBUTE
-			
-			emmetStr = emmetStr.substring(match.index + 1);
-			
-			var textMatch = /[a-zA-Z0-9_\-]+/.exec(emmetStr);
-			
-			emmetStr = emmetStr.substring(textMatch.index + textMatch[0].length);
-			
-			if (!textMatch)
-				break;
-			
-			for (var i = 0; i < nodes.length; i++) {
-				
-				var classValue = nodes[i].getAttribute('class') || '';
-				
-				classValue += ((classValue.length > 0) ? ' ' : '' ) + textMatch[0];
-				
-				nodes[i].setAttribute('class', classValue);
-			}
 
-			emmet.getOperation(nodes, emmetStr);
-			
-			break;
-		
-		case '[':
-			
-			// CUSTOM ATTRIBUTE
-			
-			emmetStr = emmetStr.substring(match.index + 1);
-			
-			var textMatch = /[^\]]+/.exec(emmetStr);
-			
-			emmetStr = emmetStr.substring(textMatch.index + textMatch[0].length);
-			
-			if (!textMatch)
-				break;
-			
-			var attributesStr = textMatch[0].trim().replace(/\s*=\s*/g, '=');
-			
-			// CONVERT SPACE IN VALUE PORTION
-			attributesStr = attributesStr.replace(/"([^"]*)"/g, function($0, $1) {
-				return $1.replace(/\s/g, '_._');
-			});
-			
-			var attributes = attributesStr.split(' ');
-			
-			for (var i = 0; i < nodes.length; i++)
-				for (var j = 0; j < attributes.length; j++) {
-					
-					var attr = attributes[j];
-					
-					var attrParts = attr.split('=');
-					
-					if (attrParts[1] == undefined)
-						attrParts[1] = '';
-					
-					// REVERT SPACE IN VALUE PORTION
-					attrParts[1] = attrParts[1].replace(/_\._/g, ' ');
-					
-					nodes[i].setAttribute(attrParts[0], attrParts[1]);
-				}
+// ==========
+// ATTRIBUTES
+// ==========
 
-			getOperation(nodes, emmetStr);
-			
-			break;
-		
-		case '{':
-			
-			// TEXT
-			
-			emmetStr = emmetStr.substring(match.index + 1);
-			
-			var textMatch = /[^\}]+/.exec(emmetStr);
-			
-			emmetStr = emmetStr.substring(textMatch.index + textMatch[0].length + 1);
-			
-			if (!textMatch)
-				break;
-			
-			for (var i = 0; i < nodes.length; i++)
-				nodes[i].appendChild(document.createTextNode(textMatch[0]));
-			
-			getOperation(nodes, emmetStr);
-			
-			break;
-		}
-		
-		// MERGE FORK
-		
-		// CLIMB-UP (PART 2)
-		if (!!nextNodes && !!nextNodes.climbup) {
-			
-			for (var i = 0; i < nextNodes.climbup.length; i++)
-				nodes.push(nextNodes.climbup[i].cloneNode(true));
-			
-			delete nextNodes.climbup;
-		}
-	}
-	
-	// ---
-	
-	function getElement(emmetStr) {
-		
-		var nodes = [];
-		
-		var match = /[\(\{a-zA-Z]/.exec(emmetStr);
-
-		if (!match)
-			return nodes;
-		
-		// FORK BASE ON (FIRST) MATCHED
-		
-		switch (match[0]) {
-		case '(':
-			
-			// GROUP
-			
-			emmetStr = emmetStr.substring(match.index + 1);
-			
-			var newEmmet = '';
-			var counter = 1;
-			
-			while(counter > 0) {
-				
-				var matchBracket = /[\(\)]/.exec(emmetStr);
-				
-				if (!matchBracket) {
-					counter = 0;
-					continue;
-				}
-				
-				if (matchBracket[0] == '(')
-					counter += 1;
-				else
-					counter -= 1;
-				
-				newEmmet += emmetStr.substring(0, matchBracket.index + 1);
-				emmetStr = emmetStr.substring(matchBracket.index + 1);
-			}
-			
-			newEmmet = newEmmet.substring(0, newEmmet.length - 1);
-			
-			nodes.push(getElement(newEmmet));
-			
-			break;
-			
-		case '{':
-			
-			// TEXT NODE
-			
-			emmetStr = emmetStr.substring(match.index + 1);
-			
-			var textMatch = /[^\}]+/.exec(emmetStr);
-			
-			if (!textMatch) {
-				
-				emmetStr = '';
-				
-			} else {
-				
-				emmetStr = emmetStr.substring(textMatch.index + textMatch[0].length + 1);
-				
-				nodes.push(document.createTextNode(textMatch[0]));
-			}
-			
-			break;
-		
-		default:
-			
-			// ELEMENTS NODE
-			
-			emmetStr = emmetStr.substring(match.index);
-			
-			var matchNameRegExp = /[a-zA-Z0-9]+/;
-			var matchName = matchNameRegExp.exec(emmetStr);
-			
-			emmetStr = emmetStr.substring(matchName.index + matchName[0].length);
-			
-			// MODIFY EMMET STRING IF ELEMENT NAME MATCHES ABBREVIATION
-			
-			if (emmetJS.abbreviation[matchName[0].toLowerCase()]) {
-				
-				emmetStr = emmetJS.abbreviation[matchName[0].toLowerCase()] + emmetStr;
-				
-				matchName = matchNameRegExp.exec(emmetStr);
-			
-				emmetStr = emmetStr.substring(matchName.index + matchName[0].length);
-			}
-			
-			nodes.push(document.createElement(matchName[0]));
-		}
-		
-		// MERGE FORK
-		
-		getOperation(nodes, emmetStr);
-		
-		return nodes;
-	}
-	
-	// ---
-	
-	return getElement(emmetStr);
-};
-
-emmetJS.abbreviation = {
-	'ah': 'a[href="#"]',
+emmet.abbreviation = {
+	'a': 'a[href="#"]',
 	'bq': 'blockquote',
 	'img': 'img[alt=""]',
+};
+
+
+// ==========
+// MAKE ELEMENTS
+// ==========
+
+emmet.make = function(abbr) {
 	
+	abbr = abbr || '';
+	
+	var nodes = [];
+	
+	var match = /[\(\{a-zA-Z]/.exec(abbr);
+	
+	if (!match)
+		return nodes;
+	
+	// FORK BASE ON (FIRST) MATCHED
+	
+	switch (match[0]) {
+	case '(':
+		
+		// GROUP
+		
+		var matchBracket = null;
+		
+		var subAbbr = '';
+		var counter = 0;
+		
+		abbr = abbr.substring(match.index);
+		
+		do {
+			
+			matchBracket = /[\(\)]/.exec(abbr);
+			
+			if (!matchBracket) {
+				counter = 0;
+				subAbbr += abbr;
+				abbr = '';
+				continue;
+			}
+			
+			switch (matchBracket[0]) {
+			case '(':
+				counter += 1;
+				break;
+			case ')':
+				counter -= 1;
+				break;
+			}
+			
+			subAbbr += abbr.substring(0, matchBracket.index + 1);
+			abbr = abbr.substring(matchBracket.index + 1);
+			
+		} while(counter > 0);
+		
+		if (matchBracket)
+			nodes = emmet.make(subAbbr.substring(1, subAbbr.length - 1));
+		
+		break;
+		
+	case '{':
+		
+		// TEXT NODE
+		
+		var textMatch = /[^\}]+/.exec(abbr);
+		
+		if (!textMatch) {
+			nodes.push(document.createTextNode(abbr.substring(match.index + 1)))
+			abbr = '';
+		} else {
+			nodes.push(document.createTextNode(textMatch[0].substring(match.index + 1)));
+			abbr = abbr.substring(textMatch[0].length + 1);
+		}
+		
+		break;
+	
+	default:
+		
+		// ELEMENTS NODE
+		
+		var matchName = /[a-zA-Z0-9]+/.exec(abbr);
+		
+		// MODIFY EMMET STRING IF ELEMENT NAME MATCHES ABBREVIATION
+		
+		var abbrMod = emmet.abbreviation[matchName[0].toLowerCase()];
+		if (abbrMod) {
+			abbr = abbrMod + abbr.substring(matchName.index + matchName[0].length);
+			matchName = /[a-zA-Z0-9]+/.exec(abbr);
+		}
+		
+		abbr = abbr.substring(matchName.index + matchName[0].length);
+		
+		nodes.push(document.createElement(matchName[0]));
+	}
+	
+	// MERGE FORK
+	
+	emmet.mod(nodes, abbr);
+	
+	return nodes;
+};
+
+
+// ==========
+// MODIFY ELEMENTS
+// ==========
+
+emmet.mod = function(nodes, abbr) {
+	
+	nodes = nodes || [];
+	
+	if (!nodes.length)
+		nodes = [nodes];
+	
+	abbr = abbr || [];
+	
+	var nextNodes = null;
+	
+	var match = /[\>\+\^\*#\.\[\{]/.exec(abbr);
+	
+	if (!match) // NO MATCH
+		return;
+		
+	// FORK BASED ON (FIRST) MATCHED
+	
+	switch (match[0]) {
+	case '>':
+	
+		// CHILD
+		
+		nextNodes = emmet.make(abbr);
+
+		for (var i = 0; i < nodes.length; i++)
+			for (var j = 0; j < nextNodes.length; j++)
+				nodes[i].appendChild(nextNodes[j].cloneNode(true));
+		
+		break;
+		
+	case '+':
+	
+		// SIBLING
+		
+		nextNodes = emmet.make(abbr);
+
+		for (var i = 0; i < nextNodes.length; i++)
+			nodes.push(nextNodes[i].cloneNode(true));
+		
+		break;
+		
+	case '^':
+	
+		// CLIMB-UP (PART 1)
+		
+		nextNodes = emmet.make(abbr);
+		
+		nodes.climbup = nextNodes;
+		
+		break;
+		
+	case '*':
+	
+		// MULTIPLICATION
+		
+		var matchParam = /[0-9]+/.exec(abbr);
+		
+		if (!matchParam)
+			break;
+		
+		var copyNodes = [];
+		
+		for (var i = 0; i < nodes.length; i++)
+			copyNodes.push(nodes[i].cloneNode(true));
+		
+		for (var i = 0; i < parseInt(matchParam[0]) - 1; i++)
+			for (var j = 0; j < copyNodes.length; j++)
+				nodes.push(copyNodes[j].cloneNode(true));
+		
+		abbr = abbr.substring(matchParam.index + matchParam[0].length);
+		
+		emmet.mod(nodes, abbr);
+		
+		break;
+	
+	case '#':
+	
+		// ID ATTRIBUTE
+		
+		abbr = abbr.substring(match.index + 1);
+		
+		var textMatch = /[a-zA-Z0-9_\-]+/.exec(abbr);
+		
+		abbr = abbr.substring(textMatch.index + textMatch[0].length);
+		
+		if (!textMatch)
+			break;
+		
+		for (var i = 0; i < nodes.length; i++)
+			nodes[i].setAttribute('id', textMatch[0]);
+		
+		emmet.mod(nodes, abbr);
+		
+		break;
+		
+	case '.':
+		
+		// CLASS ATTRIBUTE
+		
+		abbr = abbr.substring(match.index + 1);
+		
+		var textMatch = /[a-zA-Z0-9_\-]+/.exec(abbr);
+		
+		abbr = abbr.substring(textMatch.index + textMatch[0].length);
+		
+		if (!textMatch)
+			break;
+		
+		for (var i = 0; i < nodes.length; i++) {
+			
+			var classValue = nodes[i].getAttribute('class') || '';
+			
+			classValue += ((classValue.length > 0) ? ' ' : '' ) + textMatch[0];
+			
+			nodes[i].setAttribute('class', classValue);
+		}
+
+		emmet.mod(nodes, abbr);
+		
+		break;
+		
+	case '[':
+		
+		// CUSTOM ATTRIBUTE
+		
+		var attrMatch = /[^\]]+/.exec(abbr);
+		
+		if (!attrMatch)
+			break;
+		
+		var attributesStr = attrMatch[0].substring(match.index + 1).trim().replace(/\s*=\s*/g, '=');
+		
+		// TRANSFORM SPACE TO UNIQUE PATTERN
+		attributesStr = attributesStr.replace(/"([^"]*)"/g, function($0, $1) {
+			return $1.replace(/\s/g, '_._');
+		});
+		
+		var attributes = attributesStr.split(' ');
+		
+		for (var i = 0; i < nodes.length; i++)
+			for (var j = 0; j < attributes.length; j++) {
+				
+				var attr = attributes[j];
+				
+				var attrParts = attr.split('=');
+				
+				if (attrParts[1] == undefined)
+					attrParts[1] = '';
+				
+				// REVERT SPACE FROM UNIQUE PATTERN
+				attrParts[1] = attrParts[1].replace(/_\._/g, ' ');
+				
+				nodes[i].setAttribute(attrParts[0], attrParts[1]);
+			}
+		
+		emmet.mod(nodes, abbr.substring(attrMatch.index + 1));
+		
+		break;
+		
+	case '{':
+		
+		// TEXT
+		
+		var textMatch = /[^\}]+/.exec(abbr);
+		
+		var text = '';
+		
+		if (!textMatch) {
+			text = abbr.substring(match.index + 1);
+			abbr = '';
+		} else {
+			text = textMatch[0].substring(match.index + 1);
+			abbr = abbr.substring(textMatch[0].length + 1);
+		}
+		
+		for (var i = 0; i < nodes.length; i++)
+			nodes[i].appendChild(document.createTextNode(text));
+		
+		emmet.mod(nodes, abbr);
+		
+		break;
+		
+	}
+	
+	// MERGE FORK
+	
+	if (!!nextNodes && !!nextNodes.climbup) {
+		
+		// CLIMB-UP (PART 2)
+		
+		for (var i = 0; i < nextNodes.climbup.length; i++)
+			nodes.push(nextNodes.climbup[i].cloneNode(true));
+		
+		delete nextNodes.climbup;
+	}
 };
